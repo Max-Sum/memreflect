7dec1b800925aaa06919557c2518142b898e16e8
Max Sum <maxsumprc@gmail.com>
2018-03-19 01:00:17 +0800

    Modfied some details

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM alpine
 MAINTAINER Max Sum <max@lolyculture.com>
 
-# Build app
 ADD memreflect
 CMD ["./memreflect", "-p", "${MEMREFLECT_PORT:-11211}", "${MEMREFLECT_SHUTDOWN:+-s}"]
--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 go get -t github.com/max-sum/memreflect/build
 GOOS=linux GOARCH=amd64 go build -o memreflect github.com/max-sum/memreflect/build
-docker build .
+docker build --tag gzmaxsum/memreflect .
