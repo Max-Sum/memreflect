29e95dd911701a924826dc7b71f184845bf5a0fa
Max Sum <maxsumprc@gmail.com>
2018-03-19 01:09:07 +0800

    Fixed binary build

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine
 MAINTAINER Max Sum <max@lolyculture.com>
 
-ADD memreflect
+ADD memreflect .
 CMD ["./memreflect", "-p", "${MEMREFLECT_PORT:-11211}", "${MEMREFLECT_SHUTDOWN:+-s}"]
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 go get -t github.com/max-sum/memreflect/build
 GOOS=linux GOARCH=amd64 go build -o memreflect github.com/max-sum/memreflect/build
-docker build --tag gzmaxsum/memreflect .
+docker build --tag gzmaxsum/memreflect:binary .
